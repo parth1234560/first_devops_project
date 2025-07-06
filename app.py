31f4563cf0900c21af4dadf95aa0c2326a8cb895
parth1234560 <pathakparthixc7503@gmail.com>
2025-07-06 18:33:24 +0530

    first version

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/info")
+def lwinfo():
+	return "i m LW from India"
+@app.route("/phone")
+def lwphone():
+	return "9351009002"
+app.run(host="0.0.0.0")
